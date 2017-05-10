6881bfb8bb1fc63643f717496123919ffc5cff9e
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-05-10 04:18:29 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup
+from json import loads
+from time import sleep
 import requests
 
-def statsRoyale(tag):
+#9890JJJV
+#PRR2LUGO
+#9VUQUGCP
+
+def refreshProfile(tag):
+	link = 'http://statsroyale.com/profile/' + tag + '/refresh'
+	requests.get(link)
+
+def statsRoyale(tag, refresh=False):
 	if not tag.find('/') == -1:
 		tag = tag[::-1]
 		pos = tag.find('/')
@@ -9,23 +19,35 @@ def statsRoyale(tag):
 		tag = tag[::-1]
 
 	link = 'http://statsroyale.com/profile/' + tag
-	response = (requests.get(link)).text
+	response = requests.get(link).text
 	soup = BeautifulSoup(response, 'html.parser')
 
-	description = soup.find_all('div', {'class':'description'})
-	content = soup.find_all('div', {'class':'content'})
+	if refresh:
+		refreshProfile(tag)
+		sleep(20.1)
 
 	stats = {}
 
-	for i in range(len(description)):
-		description_text = ((description[i].get_text()).replace(' ', '_')).lower()
-		content_text = content[i].get_text()
-		stats[description_text] = content_text
+	try:
+		basic = soup.find('div', {'class':'statistics__name'})
+		#print(basic)
 
-	if stats['clan'] == 'No Clan':
-		stats['clan'] = None
+		stats[u'level'] = basic.find('span').get_text()
+		stats[u'username'] = max(basic.find('div').get_text().replace('\n', '').split(' '))
+
+		stats[u'profile'] = {}
+
+		profile = soup.find('div', {'class':'statistics__metrics'})
+		for div in profile.find_all('div', {'class':'statistics__metric'}):
+			result = max(div.find_all('div')[0].get_text().replace('\n', '').split(' '))
+			item = div.find_all('div')[1].get_text().replace(' ', '_').lower()
+			stats[u'profile'][item] = result
+	except:
+		pass
 
 	return stats
 
+#stats = statsRoyale(tag='9890JJJV', refresh=True)
 stats = statsRoyale(tag='9890JJJV')
-print stats
+
+print(stats)
